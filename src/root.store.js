08d2334deb8e762a8cfd7dbb69b0be13d02f1d5a
Denis Kovalev <ds.kavaliou@gmail.com>
2024-05-19 23:15:55 +0300

    feat:connect-feature-api-reducer

--- src/root.store.js
+++ src/root.store.js
@@ -1,18 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
-// import featureApi from "feature/feature-2/api";
-// import featureSlice from "feature/feature-1/slice.js";
+import charactersApi from "src/features/characters/api";
 
 const store = configureStore({
   reducer: {
-    // [featureApi.reducerPath]: featureApi.reducer,
-    // [featureSlice.reducerPath]: authSlice.reducer,
+    [charactersApi.reducerPath]: charactersApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(
-      [] // featureApi.middleware
-    );
+    return getDefaultMiddleware().concat(charactersApi.middleware);
   },
 });
 
